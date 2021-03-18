a159342124f360d9e20085927afed1becc3453c4
Nikita Ezzhev <v-niezz@microsoft.com>
2021-03-18 09:39:58 +0300

    Update PS script

--- canary/src/powershell/scripts/entrypointScript.ps1
+++ canary/src/powershell/scripts/entrypointScript.ps1
@@ -1,9 +1,10 @@
 param(
   [Parameter(Mandatory=$false)][string] $Variable1 = "Test variable value"
+  [bool] $useCallOperator
 )
 
 . $PSScriptRoot\fileWithFunction.ps1
 
 Write-Host "Variables defined in entrypoint file v1 = $Variable1"
 
-MethodInOtherScript("Script arg")
+MethodInOtherScript("Script arg", $useCallOperator)
--- canary/src/powershell/scripts/fileWithFunction.ps1
+++ canary/src/powershell/scripts/fileWithFunction.ps1
@@ -1,4 +1,4 @@
-function MethodInOtherScript([string]$arg)  { 
+function MethodInOtherScript([string]$arg, [bool]$useCallOperator)  { 
   Write-Output "Inner function called"
 
   Write-Output "Call variables from Not Global scope"
@@ -8,8 +8,17 @@ function MethodInOtherScript([string]$arg)  {
   }
 
   Write-Output "Call variables from Global scope"
-  if (Get-Variable Variable1 -Scope Global -ErrorAction SilentlyContinue)
-  { $true } else {
-    Write-Error "Variable doesn't exists in global scope" -ErrorAction Stop
+  if ($useCallOperator)
+  { 
+    if (Get-Variable Variable1 -Scope Global -ErrorAction SilentlyContinue)
+    { Write-Error "Variable shouldn't exist in global scope" -ErrorAction Stop } else {
+      $true
+    }
+  } else
+  {
+    if (Get-Variable Variable1 -Scope Global -ErrorAction SilentlyContinue)
+    { $true } else {
+      Write-Error "Variable doesn't exists in global scope" -ErrorAction Stop
+    }
   }
 }
