ea9c51ab7f1746ed444110859c144f9348438781
Nikita Ezzhev <v-niezz@microsoft.com>
2021-03-31 09:26:51 +0300

    Changed messages

--- canary/src/powershell/scripts/fileWithFunction.ps1
+++ canary/src/powershell/scripts/fileWithFunction.ps1
@@ -12,17 +12,16 @@ function MethodInOtherScript()  {
 
   Write-Output "Call variables from Not Global scope"
   if (Get-Variable Variable1 -ErrorAction SilentlyContinue)
-  { $true } else {
+  { Write-Output "Test passed, the variable is accessible in not global scope" } else {
     Write-Error "Variable doesn't exists in not global scope" -ErrorAction Stop 
   }
 
   Write-Output "Call variables from Global scope"
-  Write-Output "Use Call operator - " $runScriptInSeparateScope
   if ( $runScriptInSeparateScope )
   { 
     if (Get-Variable Variable1 -Scope Global -ErrorAction SilentlyContinue)
     { Write-Error "Variable shouldn't exist in global scope" -ErrorAction Stop } else {
-      Write-Output "Test passed, the variable is accessible from the global scope"
+      Write-Output "Test passed, the variable isn't accessible from the global scope"
     }
   } else
   {
