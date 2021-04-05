a5f868078cb30746f596570c5191e33adae58ee0
Nikita Ezzhev <v-niezz@microsoft.com>
2021-04-05 09:54:16 +0300

    Added canaries for DownloadBuildArtifactsV0 task

---
+++ canary/src/download-build-artifacts/validation_scripts/compare_folders.ps1
@@ -0,0 +1,56 @@
+<#
+    .SYNOPSIS
+        Compare original folder with downloaded folder.
+    .DESCRIPTION
+        To check the results of the "Download Build Artifacts" task
+        we need to validate what exactly was downloaded by the task.
+#>
+
+param (
+    # Path to the original folder
+    [string]$folderReference, 
+    
+    # Path to the downloaded folder
+    [string]$folderDifference
+)
+
+try { 
+    if ([string]::IsNullOrEmpty($folderReference)) {
+        throw "Path to the reference folder is empty"
+    }
+
+    if ([string]::IsNullOrEmpty($folderDifference)) {
+        throw "Path to the difference folder is empty"
+    }
+
+    Write-Host "##[command]Compare folder $folderReference with $folderDifference"
+
+    $FolderReferenceContents = Get-ChildItem $folderReference -Recurse | where-object { -not $_.PSIsContainer }
+    $FolderDifferenceContents = Get-ChildItem $folderDifference -Recurse | where-object { -not $_.PSIsContainer }
+
+    $CheckResult = Compare-Object -ReferenceObject $FolderReferenceContents -DifferenceObject $FolderDifferenceContents -Property ('Name', 'Length')
+
+    if ($CheckResult) {
+        Write-Host "##vso[task.logissue type=error;]Folders are differs - Check failed"
+
+        Write-Output "Compare result:"
+        Write-Output $CheckResult
+
+        Write-Output "Reference folder:"
+        Write-Output $FolderReferenceContents
+
+        Write-Output "Difference folder:"
+        Write-Output $FolderDifferenceContents
+
+        Write-Output "##vso[task.complete result=Failed;] Test failed"
+    }
+    else {
+        Write-Output "##[section]Folders are identical - Check passed"
+    }
+}
+catch {
+    Write-Host "##vso[task.logissue type=error;]An error occurred: $_"
+    Write-Output "##vso[task.complete result=Failed;]Test failed"
+}
+
+exit $LASTEXITCODE
---
+++ canary/src/download-build-artifacts/validation_scripts/verify_files_extensions.ps1
@@ -0,0 +1,60 @@
+<#
+    .SYNOPSIS
+        Check files extension in the folder by the whitelist.
+    .DESCRIPTION
+        The "Download Build Artifacts" task allows us to download files from 
+        several Build Artifacts that are matched some patterns. To test this 
+        functionality we download several files with the specified extensions 
+        and this script validates the results of the download.
+#>
+
+param (
+    # Path to the folder 
+    [string]$pathToFolder,
+    
+    # Array of allowed file extensions
+    # Pattern: .<extension>
+    # Example: -WhitelistedExtensions ".cpp",".hpp"
+    [String[]]$whitelistedExtensions
+)
+
+try {
+    if ([string]::IsNullOrEmpty($pathToFolder)) {
+        throw "Path to the folder is empty"
+    }
+
+    if ($whitelistedExtensions.Count -eq 0) {
+        throw "Extensions whitelist is empty"
+    }
+
+    Write-Host "##[command]Check files in $pathToFolder by following whitelist: $whitelistedExtensions"
+
+    $suspicious_files = Get-ChildItem "$pathToFolder" -Recurse | where-object { 
+        (-not $whitelistedExtensions.Contains([IO.Path]::GetExtension($_))) -and (-not $_.PSIsContainer)
+    }
+
+    if ($suspicious_files) {
+        Write-Host "##vso[task.logissue type=error;]The folder contains some not whitelisted items - Check failed"
+
+        Write-Output "Extensions whitelist:"
+        Write-Output $whitelistedExtensions
+
+        Write-Output "Suspicious files:"
+        Write-Output $suspicious_files
+
+        Write-Output "Content of $pathToFolder"
+        $folderContent = Get-ChildItem "$pathToFolder" -Recurse -Name | where-object { -not $_.PSIsContainer }
+        Write-Output $folderContent
+
+        Write-Host "##vso[task.complete result=Failed]Test failed"
+    }
+    else {
+        Write-Output "##[section]All files are passed validation - Check passed"
+    }
+}
+catch {
+    Write-Host "##vso[task.logissue type=error;]An error occurred: $_"
+    Write-Output "##vso[task.complete result=Failed;]Test failed"
+}
+
+exit $LASTEXITCODE
